a901b9080c7600a76f94e257623ca8cd3150a89e
Iodice <nickio@amazon.com>
2017-03-01 19:38:35 -0500

    cleaned up build script

--- build-all.sh
+++ build-all.sh
@@ -1,32 +1,48 @@
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+
+
 BUILD_DIR="$PWD/build/"
 
-rm -rf "$BUILD_DIR"
-mkdir -p "$BUILD_DIR"
+function runWithSuccessOrFail() {
+    $1 > /dev/null
+    RESULT=$?
+    if [ $RESULT -eq 0 ]; then
+        echo -e "$2: $COL_GREEN succeeded $COL_RESET"
+    else
+        echo -e "$2: $COL_RED failed $COL_RESET"
+    fi
+}
 
+function resetDirectory() {
+    runWithSuccessOrFail "rm -rf $1" "delete build directory"
+    runWithSuccessOrFail "mkdir -p $1" "create build directory"
+}
 
-cd config
-mvn clean
-mvn assembly:assembly
-cp target/*jar-with-dependencies.jar $BUILD_DIR
-cd ..
+function buildProject() {
+    runWithSuccessOrFail "mvn clean" "clean '$1'"
+    runWithSuccessOrFail "mvn assembly:assembly" "build '$1'"
+}
 
-mvn install:install-file \
-   -Dfile=$BUILD_DIR/config-1.0-SNAPSHOT-jar-with-dependencies.jar \
-   -DgroupId=com.iodice \
-   -DartifactId=config \
-   -Dversion=1.0 \
-   -Dpackaging=jar \
-   -DgeneratePom=true
+function copyJar() {
+    runWithSuccessOrFail "cp target/*jar-with-dependencies.jar $1" "copy jar to build directory"
+}
 
+function installMavenLocal() {
+    runWithSuccessOrFail "mvn install:install-file -Dfile=$1 -DgroupId=$2 -DartifactId=$3 -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true" "install maven jar"
+}
 
-cd crawler
-mvn clean
-mvn assembly:assembly
-cp target/*jar-with-dependencies.jar $BUILD_DIR
-cd ..
+function buildProjectAndCopyJar() {
+    cd $1
+    buildProject $1
+    copyJar $2
+    cd ..
+}
 
-cd webserver
-mvn clean
-mvn assembly:assembly
-cp target/*jar-with-dependencies.jar $BUILD_DIR
-cd ..
+resetDirectory $BUILD_DIR
+buildProjectAndCopyJar config $BUILD_DIR
+installMavenLocal $BUILD_DIR/config-1.0-SNAPSHOT-jar-with-dependencies.jar com.iodice config
+buildProjectAndCopyJar crawler $BUILD_DIR
+buildProjectAndCopyJar webserver $BUILD_DIR
