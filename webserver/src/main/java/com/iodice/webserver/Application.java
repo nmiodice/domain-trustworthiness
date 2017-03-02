f971306edaa8171f2785f3fbe18d661e28080329
Iodice <nickio@amazon.com>
2017-03-01 19:39:00 -0500

    cleaned up webserver code

--- webserver/src/main/java/com/iodice/webserver/Application.java
+++ webserver/src/main/java/com/iodice/webserver/Application.java
@@ -2,14 +2,28 @@ package com.iodice.webserver;
 
 import com.iodice.config.Config;
 import io.undertow.Undertow;
-import io.undertow.util.Headers;
+import io.undertow.server.handlers.PathHandler;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class Application {
+    private static final Log logger = LogFactory.getLog(Application.class);
+    private static final String PAGE_RANK_PATH = "/pagerank";
 
     public static void main(final String[] args) {
+        logger.info("building HTTP server");
+        PathHandler handler = new PathHandler();
+        handler.addPrefixPath(PAGE_RANK_PATH, new PageRankRequestHandler());
+
+        String host = Config.getString("server.conn.host");
+        Integer port = Config.getInt("server.conn.port");
+
         Undertow server = Undertow.builder()
-            .addHttpListener(Config.getInt("server.conn.port"), Config.getString("server.conn.host"))
-            .setHandler(new PageRankRequestHandler()).build();
+            .addHttpListener(port, host)
+            .setHandler(handler)
+            .build();
+
+        logger.info("starting HTTP server on " + host + ":" + port);
         server.start();
     }
 }
