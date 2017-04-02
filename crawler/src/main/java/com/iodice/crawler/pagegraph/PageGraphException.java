3ace6056e5768a1dcfa32f23d0da7865365dae60
Iodice <nickio@amazon.com>
2017-04-02 16:10:23 -0400

    removed unused exception type

--- crawler/src/main/java/com/iodice/crawler/pagegraph/PageGraphException.java
+++
@@ -1,7 +0,0 @@
-package com.iodice.crawler.pagegraph;
-
-public class PageGraphException extends Exception {
-    public PageGraphException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
