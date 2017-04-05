ac74e43817c4e3d77ad08b1166014fa2499a667c
Iodice <nickio@amazon.com>
2017-04-05 07:42:56 -0400

    added simple program to start java apps on remote server. script is not well done, needs to be improved but a good starting place for sure

---
+++ start-java-programs.sh
@@ -0,0 +1,18 @@
+CRAWLER_HOST="ec2-52-91-205-13.compute-1.amazonaws.com"
+WEB_SERVER_HOST="ec2-184-72-122-100.compute-1.amazonaws.com"
+
+function runCmd() {
+    ssh -i ~/.ssh/PageRank.pem ec2-user@$1 "$2"
+}
+
+function runCmdInBackground() {
+    ssh -i ~/.ssh/PageRank.pem ec2-user@$1 "nohup $2 &"
+}
+
+# runCmd $CRAWLER_HOST "yes | sudo yum install java-1.8.0"
+# runCmd $CRAWLER_HOST "yes | sudo yum remove java-1.7.0-openjdk"
+# runCmdInBackground $CRAWLER_HOST "java -jar crawler-1.0-SNAPSHOT-jar-with-dependencies.jar"
+
+# runCmd $WEB_SERVER_HOST "yes | sudo yum install java-1.8.0"
+# runCmd $WEB_SERVER_HOST "yes | sudo yum remove java-1.7.0-openjdk"
+runCmdInBackground $WEB_SERVER_HOST "sudo java -jar webserver-1.0-SNAPSHOT-jar-with-dependencies.jar"
