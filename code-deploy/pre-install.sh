1420f92a493f8153cb069f201936b49208e7a1e6
Iodice <nickio@amazon.com>
2017-04-29 08:17:25 -0400

    updated before install scripts

--- code-deploy/pre-install.sh
+++ code-deploy/pre-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-yes | yum install java-1.8.0
-yes | yum remove java-1.7.0-openjdk
-yes | yum install java-1.8.0-openjdk-devel
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+mvn --version
+
+yes | sudo yum install java-1.8.0
+yes | sudo yum remove java-1.7.0-openjdk
+yes | sudo yum install java-1.8.0-openjdk-devel
