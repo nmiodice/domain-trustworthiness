d69121f532c8bde75bc281fe3cfc290577ac8f06
Iodice <nickio@amazon.com>
2017-05-05 22:42:55 -0400

    added seed urls

--- crawler-scheduler-node/src/main/java/com/iodice/crawler/scheduler/Application.java
+++ crawler-scheduler-node/src/main/java/com/iodice/crawler/scheduler/Application.java
@@ -1,8 +1,12 @@
 package com.iodice.crawler.scheduler;
 
 import com.iodice.config.Config;
+import com.iodice.crawler.scheduler.entity.WorkRequest;
 import com.iodice.crawler.scheduler.handlers.request.RequestWorkerPool;
 import com.iodice.crawler.scheduler.handlers.response.ResponseWorkerPool;
+import com.iodice.crawler.scheduler.queue.RequestQueueAdaptor;
+
+import java.util.Collections;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,5 +16,10 @@ public class Application {
         responseWorkers.start();
         RequestWorkerPool requestWorkers = new RequestWorkerPool(Config.getInt("worker.request.worker_count"));
         requestWorkers.start();
+
+        // start workers off with a well-known seed URL
+        new RequestQueueAdaptor().emitRequest(WorkRequest.builder()
+            .urls(Config.getStringList("scheduler.seeds"))
+            .build());
     }
 }
